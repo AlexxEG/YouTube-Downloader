70ab37e4f7a1e331fbbdb38a58b4382c261261d1
Alexander Ellingsen <alexellingsen@gmail.com>
2014-03-22 14:38:33 +0100

    Update assembly information

--- YouTube Downloader/Properties/AssemblyInfo.cs
+++ YouTube Downloader/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Youtube Downloader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alexander Ellingsen")]
 [assembly: AssemblyProduct("Youtube Downloader")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
