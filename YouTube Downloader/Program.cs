e2b7b8fffb3f74889da8ecec4bdafac15d570a5c
Alexander Ellingsen <alexellingsen@gmail.com>
2014-03-27 21:57:29 +0100

    Save unhandled exceptions

--- YouTube Downloader/Program.cs
+++ YouTube Downloader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace YouTube_Downloader
@@ -11,9 +12,29 @@ namespace YouTube_Downloader
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Program.SaveException((Exception)e.ExceptionObject);
+        }
+
+        public static void SaveException(Exception ex)
+        {
+            string directory = Path.Combine(Application.StartupPath, "StackTraces");
+
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+            string file = string.Format("{0}\\StackTraces\\stackTrace.{1}.log",
+                Application.StartupPath,
+                DateTime.Now.ToString("yyyy_MM_dd-HH_mm_ss"));
+
+            File.WriteAllText(file, ex.ToString());
         }
     }
 }
