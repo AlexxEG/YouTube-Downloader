6af9af54f40c9073b2b5212ecf96e1b0ce108a8e
Alexander Ellingsen <alexellingsen@gmail.com>
2014-08-29 17:55:08 +0200

    Add more summaries to Helper

--- YouTube Downloader/Classes/Helper.cs
+++ YouTube Downloader/Classes/Helper.cs
@@ -61,11 +61,19 @@ namespace YouTube_Downloader.Classes
             }).Start();
         }
 
+        /// <summary>
+        /// Returns a formatted string of the given file size.
+        /// </summary>
+        /// <param name="size">The file size as long to format.</param>
         public static string FormatFileSize(long size)
         {
             return string.Format(new FileSizeFormatProvider(), "{0:fs}", size);
         }
 
+        /// <summary>
+        /// Returns a formatted string of the given title, stripping illegal characters and replacing HTML entities with their actual character. (e.g. &quot; -> ')
+        /// </summary>
+        /// <param name="title">The title to format.</param>
         public static string FormatTitle(string title)
         {
             string[] illegalCharacters = new string[] { "/", @"\", "*", "?", "\"", "<", ">" };
@@ -93,6 +101,10 @@ namespace YouTube_Downloader.Classes
             return sb.ToString().Trim();
         }
 
+        /// <summary>
+        /// Returns a formatted string of the video length.
+        /// </summary>
+        /// <param name="duration">The video duration as TimeSpan.</param>
         public static string FormatVideoLength(TimeSpan duration)
         {
             if (duration.Hours > 0)
@@ -101,11 +113,21 @@ namespace YouTube_Downloader.Classes
                 return string.Format("{0}:{1:00}", duration.Minutes, duration.Seconds);
         }
 
+        /// <summary>
+        /// Returns a formatted string of the video length.
+        /// </summary>
+        /// <param name="duration">The video duration as long.</param>
         public static string FormatVideoLength(long duration)
         {
             return FormatVideoLength(TimeSpan.FromSeconds(duration));
         }
 
+        /// <summary>
+        /// Calculates the ETA.
+        /// </summary>
+        /// <param name="speed">The speed as bytes. (Bytes per second?)</param>
+        /// <param name="totalBytes">The total amount of bytes.</param>
+        /// <param name="downloadedBytes">The amount of downloaded bytes.</param>
         public static long GetETA(int speed, long totalBytes, long downloadedBytes)
         {
             if (speed == 0)
@@ -116,6 +138,10 @@ namespace YouTube_Downloader.Classes
             return remainBytes / speed;
         }
 
+        /// <summary>
+        /// Returns a formatted string of file size from given file.
+        /// </summary>
+        /// <param name="file">The file to get file size from.</param>
         public static string GetFileSize(string file)
         {
             FileInfo info = new FileInfo(file);
@@ -123,6 +149,10 @@ namespace YouTube_Downloader.Classes
             return Helper.FormatFileSize(info.Length);
         }
 
+        /// <summary>
+        /// Returns the playlist id from given url.
+        /// </summary>
+        /// <param name="url">The url to get playlist id from.</param>
         public static string GetPlaylistId(string url)
         {
             Regex regex = new Regex("https?://www.youtube.com/.*[playlist|watch].*list=(\\w+)");
@@ -130,6 +160,10 @@ namespace YouTube_Downloader.Classes
             return regex.Match(url).Groups[1].Value;
         }
 
+        /// <summary>
+        /// Returns the DASH audio format from the given VideoInfo.
+        /// </summary>
+        /// <param name="video">The video to get audio format from.</param>
         public static VideoFormat GetAudioFormat(VideoInfo video)
         {
             foreach (VideoFormat f in video.Formats)
@@ -140,6 +174,11 @@ namespace YouTube_Downloader.Classes
             return null;
         }
 
+        /// <summary>
+        /// Returns the prefered video format quality based on application setting.
+        /// </summary>
+        /// <param name="video">The video to get format from.</param>
+        /// <param name="dash">True to only get DASH formats.</param>
         public static VideoFormat GetPreferedFormat(VideoInfo video, bool dash)
         {
             VideoFormat[] qualities = Helper.GetVideoFormats(video, dash);
@@ -174,6 +213,11 @@ namespace YouTube_Downloader.Classes
             return qualities[index];
         }
 
+        /// <summary>
+        /// Returns a list of formats from the given VideoInfo, excluding audio & DASH formats if dash is false.
+        /// </summary>
+        /// <param name="video">The video to get formats from.</param>
+        /// <param name="dash">True to only get DASH formats.</param>
         public static VideoFormat[] GetVideoFormats(VideoInfo video, bool dash)
         {
             var formats = new List<VideoFormat>();
@@ -201,6 +245,10 @@ namespace YouTube_Downloader.Classes
             return formats.ToArray();
         }
 
+        /// <summary>
+        /// Returns true if the given url is a playlist YouTube url.
+        /// </summary>
+        /// <param name="url">The url to check.</param>
         public static bool IsPlaylist(string url)
         {
             Regex regex = new Regex("https?://www.youtube.com/.*[playlist|watch].*list=(\\w+)");
@@ -208,6 +256,10 @@ namespace YouTube_Downloader.Classes
             return regex.IsMatch(url);
         }
 
+        /// <summary>
+        /// Returns true if the given url is a valid YouTube url.
+        /// </summary>
+        /// <param name="url">The url to check.</param>
         public static bool IsValidYouTubeUrl(string url)
         {
             if (!url.ToLower().Contains("www.youtube.com/watch?"))
