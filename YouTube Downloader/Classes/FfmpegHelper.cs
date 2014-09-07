6c6a2ce104bd050f098b93b3a9d917b14b9a127a
Alexander Ellingsen <alexellingsen@gmail.com>
2014-09-07 22:31:37 +0200

    Organize usings

--- YouTube Downloader/Classes/FfmpegHelper.cs
+++ YouTube Downloader/Classes/FfmpegHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Threading;
 using System.Windows.Forms;
 
 namespace YouTube_Downloader.Classes
