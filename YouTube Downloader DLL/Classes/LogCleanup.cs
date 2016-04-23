9cf768d13b7e4e4e649887792d87f7004ae3f9ad
Alexander Ellingsen <alexellingsen@gmail.com>
2016-04-23 15:54:29 +0200

    Add missing file for log cleanup

---
+++ YouTube Downloader DLL/Classes/LogCleanup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace YouTube_Downloader_DLL.Classes
+{
+    public class LogCleanup
+    {
+        /// <summary>
+        /// Folders to cleanup.
+        /// </summary>
+        public static string[] Folders = new string[]
+        {
+            Common.GetJsonDirectory(),
+            Common.GetLogsDirectory(),
+            Common.GetStackTracesDirectory()
+        };
+
+        /// <summary>
+        /// Max log age. Default is 14 days.
+        /// </summary>
+        public static TimeSpan MaxLogAge = new TimeSpan(14, 0, 0, 0);
+
+        public static async void RunAsync()
+        {
+            await Task.Run(delegate
+            {
+                var cleanupList = new List<string>();
+
+                foreach (string dir in Folders)
+                {
+                    var dirInfo = new DirectoryInfo(dir);
+
+                    if (!dirInfo.Exists)
+                        continue;
+
+                    foreach (var file in dirInfo.GetFiles("*.*", SearchOption.AllDirectories))
+                    {
+                        var age = DateTime.Today.Subtract(file.CreationTime);
+
+                        if (age >= MaxLogAge)
+                            cleanupList.Add(file.FullName);
+                    }
+                }
+
+                if (cleanupList.Count > 0)
+                    Helper.DeleteFiles(cleanupList.ToArray());
+            });
+        }
+    }
+}
