1b3bff70509f6ca9858df8f584ff3ef9244d27b3
Alexander Ellingsen <alexellingsen@gmail.com>
2016-04-26 02:03:17 +0200

    Implement custom event args for FileDownloadFailed event

---
+++ YouTube Downloader DLL/Classes/FileDownloadFailedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace YouTube_Downloader_DLL.Classes
+{
+    public class FileDownloadFailedEventArgs : EventArgs
+    {
+        public Exception Exception { get; private set; }
+        public FileDownload FileDownload { get; private set; }
+
+        public FileDownloadFailedEventArgs(Exception exception, FileDownload fileDownload)
+        {
+            this.Exception = exception;
+            this.FileDownload = fileDownload;
+        }
+    }
+}
