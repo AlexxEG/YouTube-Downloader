f6a6b179a6a4bbd48c9b0858348d286fd9c7ff66
Alexander Ellingsen <alexellingsen@gmail.com>
2016-10-01 19:54:06 +0200

    Implement ExceptionDialog

---
+++ YouTube Downloader/Dialogs/ExceptionDialog.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace YouTube_Downloader.Dialogs
+{
+    partial class ExceptionDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lText = new System.Windows.Forms.Label();
+            this.llToggleDetails = new System.Windows.Forms.LinkLabel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lText, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(204, 116);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 2);
+            this.panel1.Controls.Add(this.llToggleDetails);
+            this.panel1.Controls.Add(this.btnOK);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 67);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(204, 49);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(104, 11);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(88, 26);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // lText
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.lText, 2);
+            this.lText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lText.Location = new System.Drawing.Point(0, 0);
+            this.lText.Margin = new System.Windows.Forms.Padding(0);
+            this.lText.Name = "lText";
+            this.lText.Padding = new System.Windows.Forms.Padding(10);
+            this.lText.Size = new System.Drawing.Size(204, 67);
+            this.lText.TabIndex = 2;
+            this.lText.Text = "Test";
+            this.lText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // llToggleDetails
+            // 
+            this.llToggleDetails.AutoSize = true;
+            this.llToggleDetails.Location = new System.Drawing.Point(12, 18);
+            this.llToggleDetails.Name = "llToggleDetails";
+            this.llToggleDetails.Size = new System.Drawing.Size(69, 13);
+            this.llToggleDetails.TabIndex = 1;
+            this.llToggleDetails.TabStop = true;
+            this.llToggleDetails.Text = "Show Details";
+            this.llToggleDetails.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llToggleDetails_LinkClicked);
+            // 
+            // ExceptionDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(204, 116);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(154, 155);
+            this.Name = "ExceptionDialog";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ExceptionDialog";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lText;
+        private System.Windows.Forms.LinkLabel llToggleDetails;
+    }
+}
---
+++ YouTube Downloader/Dialogs/ExceptionDialog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YouTube_Downloader.Dialogs
+{
+    public partial class ExceptionDialog : Form
+    {
+        const int ExtraHeight = 60;
+        const int MaximumWidth = 494;
+
+        public ExceptionDialog()
+        {
+            InitializeComponent();
+            lText.Font = SystemFonts.MessageBoxFont;
+        }
+
+        public ExceptionDialog(string text, string caption)
+            : this()
+        {
+            this.Text = caption;
+            lText.Text = text;
+            this.FitText();
+        }
+
+        private void llToggleDetails_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void FitText()
+        {
+            var size = this.CreateGraphics().MeasureString(lText.Text,
+                                                           lText.Font,
+                                                           MaximumWidth - (lText.Padding.Left + lText.Padding.Right)
+                                                          ).ToSize();
+            var height = this.Height - lText.Height;
+            var width = this.Width - lText.Width;
+
+            this.Height = height + size.Height + ExtraHeight;
+            this.Width = width + size.Width;
+        }
+
+        public static void ShowDialog(string text, string caption)
+        {
+            new ExceptionDialog(text, caption).ShowDialog();
+        }
+    }
+}
